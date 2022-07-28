81ee92c6397ffa3a06e974c72e1b40c4c6b45df7
Elijah Noble <elijahnobledev@gmail.com>
2022-07-27 22:52:35 -0700

    Assiginment Completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
 
-end
+    def name=(dogs_name)
+      @this_dogs_name = dogs_name
+    end
+  
+    def name
+      @this_dogs_name
+    end
+  end
+
+odie = Dog.new
+odie.name = "Odie"
+
+# call the Dog#name method
+puts odie.name
+# "Odie"
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
